df12bf6662c02c999b261c19418f0062b9676185
Eugene Fedorenko <eugene.fedorenko@eproseed.com>
2016-07-18 17:09:20 +0300

    Ьщму длытвлдйцт

---
+++ MainProject/src/com/adfpractice/gittraining_test/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.adfpractice.gittraining;
+
+
+
+public class HelloWorld
+{
+  public HelloWorld()
+  {
+    super();
+  }
+
+
+
+
+
+    
+    
+ //A very simple method
+ //The forgotten line
+
+
+ public void sayHello()
+  {
+    System.out.println("Hello World!!!");
+  }
+
+  public static void main(String[] args)
+  {
+    HelloWorld helloWorld = new HelloWorld();
+    helloWorld.sayHello();
+  }
+}
+    
